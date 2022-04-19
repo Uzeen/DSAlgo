0bf857d24101e39ed7ee531a40730cce4c39140d
Uzeen Chhabra <uzeen619@gmail.com>
2022-04-19 23:40:10 +0530

    Selection sort

--- com/ds/Sorting/Sorting.java
+++ com/ds/Sorting/Sorting.java
@@ -2,15 +2,14 @@ package com.ds.Sorting;
 
 public class Sorting {
     
+    // TC O[n^2]
     public static void bubbleSort(int[] arr){
         int n = arr.length;
         for(int i=0;i<n;i++){
             int spare = 0;
             for(int j=1;j<n-i;j++){
                 if(arr[spare] > arr[j]){
-                    int temp = arr[spare];
-                    arr[spare] = arr[j];
-                    arr[j] = temp;
+                    swap(arr,spare,j);
                 }
                 spare++;
             }
@@ -18,7 +17,19 @@ public class Sorting {
     }
 
     //
-    public static void selectionSort(int[] arr){}
+    public static void selectionSort(int[] arr){
+        int n = arr.length;
+        for(int i=0;i<n;i++){
+            int minIdx = i;
+            for(int j=i+1;j<n;j++){
+                if(arr[minIdx] > arr[j]){
+                    minIdx = j;
+                }
+            }
+            swap(arr, i, minIdx);
+        }
+
+    }
 
     //
     public static void insertionSort(int[] arr){}
@@ -31,4 +42,10 @@ public class Sorting {
 
     //
     public static void quickSort(){}
+
+    private static void swap(int[] arr,int a, int b){
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
 }
